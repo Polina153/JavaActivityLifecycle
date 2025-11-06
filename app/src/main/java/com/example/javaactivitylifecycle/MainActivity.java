28910e909ade33b869eca7ce20357f49551b6723
Linka <Polina153@mail.ru>
2025-11-06 18:41:19 +0400

    Initial commit

---
+++ app/src/main/java/com/example/javaactivitylifecycle/MainActivity.java
@@ -0,0 +1,190 @@
+package com.example.javaactivitylifecycle;
+
+import static android.content.ContentValues.TAG;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Locale;
+
+/*public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        String instanceState;
+        if (savedInstanceState == null) {
+            instanceState = "Первый запуск!";
+        } else {
+            instanceState = "Повторный запуск!";
+        }
+        //Toast.makeText(this, instanceState + " - onCreate()", Toast.LENGTH_SHORT).show();
+        makeToast(instanceState + " - onCreate()");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        //Toast.makeText(getApplicationContext(), "onStart()", Toast.LENGTH_SHORT).show();
+        makeToast("onStart()");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle saveInstanceState) {
+        super.onRestoreInstanceState(saveInstanceState);
+        //Toast.makeText(getApplicationContext(), "Повторный запуск!! - onRestoreInstanceState()", Toast.LENGTH_SHORT).show();
+        makeToast("Повторный запуск!! - onRestoreInstanceState()");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //Toast.makeText(getApplicationContext(), "onResume()", Toast.LENGTH_SHORT).show();
+        makeToast("onResume()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //Toast.makeText(getApplicationContext(), "onPause()", Toast.LENGTH_SHORT).show();
+        makeToast("onPause()");
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle saveInstanceState) {
+        super.onSaveInstanceState(saveInstanceState);
+        //Toast.makeText(getApplicationContext(), "onSaveInstanceState()", Toast.LENGTH_SHORT).show();
+        makeToast("onSaveInstanceState()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        //Toast.makeText(getApplicationContext(), "onStop()", Toast.LENGTH_SHORT).show();
+        makeToast("onStop()");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        //Toast.makeText(getApplicationContext(), "onRestart()", Toast.LENGTH_SHORT).show();
+        makeToast("onRestart()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //Toast.makeText(getApplicationContext(), "onDestroy()", Toast.LENGTH_SHORT).show();
+        makeToast("onDestroy()");
+    }
+
+
+    private void makeToast(String message){
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+        Log.d(TAG, message);
+    }
+}*/
+
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Counters counters;
+
+    private TextView textCounter1;  // пользовательский элемент 1-го счетчика
+    private TextView textCounter2;  // пользовательский элемент 2-го счетчика
+    private TextView textCounter3;  // пользовательский элемент 3-го счетчика
+    private TextView textCounter4;  // пользовательский элемент 4-го счетчика
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main2);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        counters = new Counters();
+        initView();
+    }
+
+    private void initButton4ClickListener() {
+        Button button4 = findViewById(R.id.button4);
+        button4.setOnClickListener(button4ClickListener);
+    }
+
+    private void initButton3ClickListener() {
+        Button button3 = findViewById(R.id.button3);
+        button3.setOnClickListener(this);
+    }
+
+    private void initButton2ClickListener() {
+        Button button2 = findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                counters.incrementCounter1();
+                setTextCounter(textCounter2, counters.getCounter1());
+            }
+        });
+    }
+
+    // Обработка кнопки через атрибут onClick в макете
+    public void button1_onClick(View view) {
+        counters.incrementCounter2();
+        setTextCounter(textCounter1, counters.getCounter2());
+    }
+
+    @Override
+    public void onClick(View v) {
+        // Если на экране один пользовательский элемент, то такая обработка имеет смысл,
+        // но если на экране несколько элементов, здесь придется создавать "лишние" условия.
+        counters.incrementCounter3();
+        setTextCounter(textCounter3, counters.getCounter3());
+    }
+
+    public View.OnClickListener button4ClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            counters.incrementCounter4();
+            setTextCounter(textCounter4, counters.getCounter4());
+        }
+    };
+
+    // Установить текст на TextView
+    private void setTextCounter(TextView textCounter, int counter) {
+        textCounter.setText(String.format(Locale.getDefault(), "%d", counter));
+    }
+
+    private void initView() {
+        // Получить пользовательские элементы по идентификатору
+        textCounter1 = findViewById(R.id.textView1);
+        textCounter2 = findViewById(R.id.textView2);
+        textCounter3 = findViewById(R.id.textView3);
+        textCounter4 = findViewById(R.id.textView4);
+
+        initButton2ClickListener();
+        initButton3ClickListener();
+        initButton4ClickListener();
+    }
+}
